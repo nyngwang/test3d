b9a310983554223bcff4f506f69bb4541e1254aa
nyngwang <nyngwang@gmail.com>
2022-02-15 07:02:12 +0800

    1

---
+++ Assets/test1.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class test1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+      Debug.Log("hello world");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
